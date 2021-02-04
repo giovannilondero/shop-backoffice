c06b4aa55d513f80141970aefdebca087906c673
Giovanni Londero <londero.giovanni@gmail.com>
2021-02-04 22:57:22 +0100

    fix: change Product struct, API docs are wrong

--- domain/product.ts
+++ domain/product.ts
@@ -1,6 +1,11 @@
 import Employee from './employee';
 
 interface Product {
+  id: string;
+  data: ProductData;
+}
+
+interface ProductData {
   title: string;
   category: string;
   price: number;
