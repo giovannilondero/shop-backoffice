ca0a8b9f726c6d9a28909c1a582b958d480e9b2f
Giovanni Londero <londero.giovanni@gmail.com>
2021-02-06 14:49:01 +0100

    feat: set app container with max-width for content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
+import { Container } from '@material-ui/core';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return (
+    <Container maxWidth="lg">
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </Container>
+  );
 }
 
 export default MyApp;
